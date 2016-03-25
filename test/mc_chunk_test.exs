89ed617f3161f30637d10d492365f9e72513df5b
Gjum <code.gjum@gmail.com>
2016-03-25 02:02:56 +0100

    add test for palette decoding

--- test/mc_chunk_test.exs
+++ test/mc_chunk_test.exs
@@ -2,10 +2,6 @@ defmodule McChunkTest do
   use ExUnit.Case
   doctest Chunk
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-
   test "load chunk and check some blocks" do
     bit_mask_in = 0b1111111 # 127
     bin_in = File.read! "chunks/chunks/chunk_-10_5_1457918629636.dump"
@@ -22,8 +18,19 @@ defmodule McChunkTest do
     # TODO check some blocks
   end
 
+  test "palette decoding" do
+    {[], ""} = Palette.decode(<<0>>)
+    {[1, 2, 3], ""} = Palette.decode(<<3, 1, 2, 3>>)
+
+    # length value larger than one byte
+    {palette, ""} = Palette.decode(<<0b1010110000000010::16, 0::2400>>)
+    assert length(palette) == 300
+    assert Enum.all? Enum.map palette, &(&1 == 0)
+  end
+
   test "palette encoding" do
     # <<0>> = Palette.encode([])
+    # TODO
   end
 
 end
